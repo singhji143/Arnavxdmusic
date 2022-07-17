68136d17eb1761f075ed25585f157c916d489a3a
Arnav singh <108041018+singhji143@users.noreply.github.com>
2022-07-17 07:17:59 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -25,7 +25,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply("ᴡᴀɪᴛ ʙᴀʙʏ😘, ᴀʀɴᴀᴠᴊɪ sᴇᴀʀᴄʜɪɴɢ ʏᴏᴜʀ ǫᴜᴀʀʏ ғʀᴏᴍ ᴅᴇᴛᴀʙᴀsᴇ👩‍💻...")
+    m = message.reply("ᴡᴀɪᴛ ʙᴀʙʏ, ᴀʀɴᴀᴠᴊɪ sᴇᴀʀᴄʜɪɴɢ ʏᴏᴜʀ ǫᴜᴀʀʏ ғʀᴏᴍ ᴅᴇᴛᴀʙᴀsᴇ👩‍💻...")
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
